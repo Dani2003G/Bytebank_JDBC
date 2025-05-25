8b17ce52ca10478d219733ee54ce46f305b1106d
Daniel Gonçalves De Almeida <daniel0202almeida@outlook.com>
2025-05-25 13:33:57 -0300

    Usando o HikariCP para limitar o número maximo de conexao que podem ficar abertas

--- src/main/java/br/com/alura/bytebank/ConnectionFactory.java
+++ src/main/java/br/com/alura/bytebank/ConnectionFactory.java
@@ -1,9 +1,11 @@
 package br.com.alura.bytebank;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Properties;
 
@@ -11,22 +13,30 @@ public class ConnectionFactory {
 
     public Connection recuperarConexao() {
         try {
-            Properties props = new Properties();
-            InputStream input = ConnectionFactory.class.getClassLoader().getResourceAsStream("config.properties");
-            if (input == null) {
-                throw new RuntimeException("Arquivo config.properties não encontrado no classpath.");
-            }
-            props.load(input);
-
-            String url = props.getProperty("db.url");
-            String user = props.getProperty("db.user");
-            String password = props.getProperty("db.password");
-
-            return DriverManager
-                    .getConnection(url, user, password);
+            return createDataSource().getConnection();
         } catch (SQLException | IOException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    private HikariDataSource createDataSource() throws IOException {
+        Properties props = new Properties();
+        InputStream input = ConnectionFactory.class.getClassLoader().getResourceAsStream("config.properties");
+        if (input == null) {
+            throw new RuntimeException("Arquivo config.properties não encontrado no classpath.");
+        }
+        props.load(input);
+
+        String url = props.getProperty("db.url");
+        String user = props.getProperty("db.user");
+        String password = props.getProperty("db.password");
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(url);
+        config.setUsername(user);
+        config.setPassword(password);
+        config.setMaximumPoolSize(10);
+        return new HikariDataSource(config);
+
     }
 
 }
